fc0f6f297657ce1821fd50a8f5d9c0ca462ad328
Gvte-Kali <ton@email.com>
2026-04-27 12:41:12 +0200

    refactor: suppression secrets.json, nettoyage config

--- database.py
+++ database.py
@@ -10,7 +10,7 @@ def get_connection():
     return pymysql.connect(
         host=os.getenv("DB_HOST", "127.0.0.1"),
         port=int(os.getenv("DB_PORT", 3306)),
-        user=os.getenv("DB_USER", "avea"),
+        user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME", "asset_vuln_manager"),
         charset="utf8mb4",
